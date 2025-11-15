121fc2cbfa6d28514c443dbec8d73fe019a8290e
Daniel Salas Alonso <dsa069@inlumine.ual.es>
2025-11-15 14:06:08 +0000

    Plantilla web apache 3.6

--- main.tf
+++ main.tf
@@ -14,6 +14,24 @@ resource "openstack_compute_instance_v2" "tf_vm" {
   user_data = file("install_mysql.sh")
 }
 
+#Crear nodo appserver
+resource "openstack_compute_instance_v2" "appserver" {
+  name              = "appserver"
+  image_name        = "ubuntu24.04"
+  availability_zone = "nova"
+  flavor_name       = "m1.medium"
+  key_pair          = var.openstack_keypair
+  security_groups   = ["default"]
+  network {
+    name = var.openstack_network_name
+  }
+
+    user_data = templatefile("${path.module}/install_appserver.tpl", { mysql_ip = openstack_compute_instance_v2.tf_vm.network.0.fixed_ip_v4 })
+
+  depends_on = [openstack_compute_instance_v2.tf_vm]
+
+}
+
 #Creación de un recurso dirección IP flotante. El objeto recurso creado es asignado a la variable tf_vm_ip.
 resource "openstack_networking_floatingip_v2" "tf_vm_ip" {
   pool = "public1"
